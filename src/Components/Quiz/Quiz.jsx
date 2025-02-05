5424bdf47e7006af1821c8d753ca49d09afe2445
Dmytro Popov <popov.dimitriy@gmail.com>
2025-02-05 22:37:43 +0200

    Quiz

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,97 @@
+/* eslint-disable no-console */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable react/prop-types */
+/* eslint-disable jsx-a11y/alt-text */
+import { useState } from "react";
+import styles from "./Quiz.module.css";
+
+const questions = [
+  {
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
+    correct: 0,
+  },
+  {
+    title: "Компонент - это ... ",
+    variants: [
+      "приложение",
+      "часть приложения или страницы",
+      "то, что я не знаю что такое",
+    ],
+    correct: 1,
+  },
+  {
+    title: "Что такое JSX?",
+    variants: [
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
+    ],
+    correct: 2,
+  },
+];
+
+function Result({ correct }) {
+  return (
+    <div className={styles.result}>
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>
+        Вы отгадали {correct} ответа из {questions.length}
+      </h2>
+      <a href="/dist">
+        <button> Попробовать снова </button>
+      </a>
+    </div>
+  );
+}
+
+function Game({ step, question, onClickVariant }) {
+  const percentage = Math.round((step / questions.length) * 100);
+  console.log(percentage);
+
+  return (
+    <div>
+      <div className={styles.progress}>
+        <div style={{ width: `${percentage}%` }} className={styles.inner}></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants.map((text, index) => (
+          <li onClick={() => onClickVariant(index)} key={text}>
+            {text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function AppQuiz() {
+  const [step, setStep] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const question = questions[step];
+  console.log(question);
+
+  const onClickVariant = (index) => {
+    console.log(step, index);
+    setStep(step + 1);
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+    }
+  };
+
+  return (
+    <div className={styles.App}>
+      {step !== questions.length ? (
+        <Game step={step} question={question} onClickVariant={onClickVariant} />
+      ) : (
+        <Result correct={correct} />
+      )}
+    </div>
+  );
+}
+
+export default AppQuiz;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import AppQuiz from "./Components/Quiz/Quiz";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
-    </React.StrictMode>
+    <AppQuiz />
+  </React.StrictMode>
 );
-
-
