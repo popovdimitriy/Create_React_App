bcd891f28ebe21660c2be691a44a7690f8162e12
Dmytro Popov <popov.dimitriy@gmail.com>
2025-02-05 23:47:40 +0200

    dtyu

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import AppUsers from "./Components/Users/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
-    </React.StrictMode>
+    <AppUsers />
+  </React.StrictMode>
 );
-
-
