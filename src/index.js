2a59ae6e29e254cf00390d0a77db43403138b878
Dmytro Popov <popov.dimitriy@gmail.com>
2025-02-05 22:20:59 +0200

    Currency_convertor

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import AppCurrency from "./Components/Currency_convertor/AppCurrency";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
-    </React.StrictMode>
+    <AppCurrency />
+  </React.StrictMode>
 );
-
-
