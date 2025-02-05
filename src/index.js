6bfc1611737de50eefcade15c259d34949101ee2
Dmytro Popov <popov.dimitriy@gmail.com>
2025-02-05 22:18:18 +0200

    counter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Counter from "./Components/Counter/Counter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
-    </React.StrictMode>
+    <Counter />
+  </React.StrictMode>
 );
-
-
